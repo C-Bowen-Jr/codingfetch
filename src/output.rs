14627646605b9b7ed2aa12c6c60fdd5719fa174e
Chris Bowen Jr <laylong@gmx.com>
2023-07-30 12:34:17 -0400

    NodeJS Added Feature:  - NodeJS checked

--- src/output.rs
+++ src/output.rs
@@ -147,6 +147,21 @@ pub fn main(matches: ArgMatches) {
             Err(_) => (),
         };
 
+        let nodejs = std::process::Command::new("nodejs")
+            .arg("--version")
+            .output();
+        match nodejs {
+            Ok(nodejs_version) => {
+                let nodejs_version_string = std::str::from_utf8(&nodejs_version.stdout).expect("");
+                let captured_version = reg_find_version.captures(&nodejs_version_string);
+                match captured_version {
+                    Some(found_version) => language_chart.push(VersionChart::new("NodeJS".to_string(),found_version[1].to_string())),
+                    None => (),
+                }
+            },
+            Err(_) => (),
+        };
+
         let temp_logo = [
             Logo::new("    **    ".to_string()),
             Logo::new("   *--*   ".to_string()),
